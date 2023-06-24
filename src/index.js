807fcb9c53db4a62a34262579954eef14fa9396f
kaushal-github-01 <kaushalkundu5683@gmail.com>
2023-06-24 13:52:16 +0530

    added switching between home and menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,78 @@
-import homepage from "./homepage.js";
-import menu from "./menu.js";
+import homepage from "./homepage";
+import menu from "./menu";
 
-// homepage();
-menu();
+homepage();
+
+let currentPage = "homepage";
+
+const Home = content.children[1].children[1];
+const Menu = content.children[1].children[2];
+const About = content.children[1].children[3];
+const Login = content.children[1].children[4];
+
+Home.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentPage !== "homepage") {
+    currentPage = "homepage";
+    content.innerHTML = "";
+    homepage();
+    attachEventListeners();
+  }
+});
+
+Menu.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentPage !== "menu") {
+    currentPage = "menu";
+    content.innerHTML = "";
+    menu();
+    attachEventListeners();
+  }
+});
+
+function attachEventListeners() {
+  const Home = content.children[1].children[1];
+  const Menu = content.children[1].children[2];
+  const About = content.children[1].children[3];
+  const Login = content.children[1].children[4];
+
+  Home.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (currentPage !== "homepage") {
+      currentPage = "homepage";
+      content.innerHTML = "";
+      homepage();
+      attachEventListeners();
+    }
+  });
+
+  Menu.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (currentPage !== "menu") {
+      currentPage = "menu";
+      content.innerHTML = "";
+      menu();
+      attachEventListeners();
+    }
+  });
+
+  About.addEventListener("click", (e) => {
+    e.preventDefault();
+    // if (currentPage !== "about") {
+    //   currentPage = "about";
+    //   content.innerHTML = "";
+    //   about();
+    //   attachEventListeners();
+    // }
+  });
+
+  Login.addEventListener("click", (e) => {
+    e.preventDefault();
+    // if (currentPage !== "login") {
+    //   currentPage = "login";
+    //   content.innerHTML = "";
+    //   login();
+    //   attachEventListeners();
+    // }
+  });
+}
